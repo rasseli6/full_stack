d488964d79b2c32cfd9f5e6db1937fd8d3464d70
Rasmus Harmanen <rasmus@harmanen.fi>
2026-04-11 13:16:49 +0300

    Osa3-2 renderille

--- osa3/notes-backend/index.js
+++ osa3/notes-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-
+app.use(express.static('dist'))
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
