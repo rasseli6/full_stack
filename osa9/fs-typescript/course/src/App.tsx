5ca2998878d955eb10764747d0b8f37d4ffbdd5f
Rasmus Harmanen <rasmus@harmanen.fi>
2026-05-02 13:53:03 +0300

    osa9 tehtavat 18 asti ok

---
+++ osa9/fs-typescript/course/src/App.tsx
@@ -0,0 +1,180 @@
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: 'special';
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial;
+
+interface HeaderProps {
+  name: string;
+}
+
+const Header = ({ name }: HeaderProps) => {
+  return <h1>{name}</h1>;
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
+
+interface PartProps {
+  part: CoursePart;
+}
+
+const Part = ({ part }: PartProps) => {
+  switch (part.kind) {
+    case 'basic':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+        </p>
+      );
+
+    case 'group':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          project exercises {part.groupProjectCount}
+        </p>
+      );
+
+    case 'background':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+          <br />
+          submit to {part.backgroundMaterial}
+        </p>
+      );
+
+    case 'special':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+          <br />
+          required skills: {part.requirements.join(', ')}
+        </p>
+      );
+
+    default:
+      return assertNever(part);
+  }
+};
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+const Content = ({ courseParts }: ContentProps) => {
+  return (
+    <>
+      {courseParts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
+    </>
+  );
+};
+
+interface TotalProps {
+  courseParts: CoursePart[];
+}
+
+const Total = ({ courseParts }: TotalProps) => {
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0,
+  );
+
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = 'Half Stack application development';
+
+  const courseParts: CoursePart[] = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+      description: 'This is an awesome course part',
+      kind: 'basic',
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: 'group',
+    },
+    {
+      name: 'Basics of type Narrowing',
+      exerciseCount: 7,
+      description: 'How to go from unknown to string',
+      kind: 'basic',
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+      description: 'Confusing description',
+      backgroundMaterial: 'https://type-level-typescript.com/template-literal-types',
+      kind: 'background',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      kind: 'special',
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
